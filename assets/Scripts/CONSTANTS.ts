39f55b2c0f991f5f9104771774e10512a43716cb
vuongcdt <vuongcdt@gmail.com>
2024-10-29 18:28:41 +0700

    add startRotation of Projectile

---
+++ assets/Scripts/CONSTANTS.ts
@@ -0,0 +1,5 @@
+
+export const SHOOT: string = 'SHOOT';
+
+
+
--- assets/Scripts/Projectile.ts
+++ assets/Scripts/Projectile.ts
@@ -1,14 +1,37 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Quat, Tween, tween, v3, Vec3 } from 'cc';
+import { eventTarget } from './Common';
+import { SHOOT } from './CONSTANTS';
 const { ccclass, property } = _decorator;
 
 @ccclass('Projectile')
 export class Projectile extends Component {
-    start() {
+    @property
+    private speed: number = 0;
 
+    private _centerPoint: Vec3 = Vec3.ZERO;
+
+    start() {
+        eventTarget.on(SHOOT, () => this.shootProjectile());
+
+        this.node.position = this._centerPoint;
+        this.startRotation();
     }
 
-    update(deltaTime: number) {
-        
+    startRotation() {
+        const duration = 10 / this.speed;
+
+        tween(this.node)
+            .to(duration, { eulerAngles: v3(0, 0, 360) })
+            .call(() => {
+                this.node.setRotationFromEuler(0, 0, 0);
+                this.startRotation();
+            })
+            .start();
+    }
+
+    shootProjectile() {
+
     }
 }
-
+
+
